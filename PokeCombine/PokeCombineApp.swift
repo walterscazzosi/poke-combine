cd5e94be48e646dfe353b04174a52d96dce780f8
Walter Scazzosi <scazzosi.walter@gmail.com>
2026-05-22 16:26:46 +0200

    First commit

---
+++ PokeCombine/PokeCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeCombineApp.swift
+//  PokeCombine
+//
+//  Created by Scazzosi Walter on 22/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct PokeCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
